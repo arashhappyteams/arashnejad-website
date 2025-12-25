fd25a0bc70a67a61ed7042c336b1f066f208f443
arashhappyteams <arash@happyteams.ai>
2025-12-25 13:40:09 -0800

    Replace SVG signature with image

--- src/components/Signature.tsx
+++ src/components/Signature.tsx
@@ -1,21 +1,9 @@
 export function Signature() {
   return (
-    <svg
-      width="120"
-      height="40"
-      viewBox="0 0 120 40"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className="block h-6 w-auto opacity-70"
-    >
-      <path
-        d="M10 25 Q 15 20, 20 25 T 35 25 M 35 15 L 35 30 M 45 20 Q 50 15, 55 20 T 65 20 M 70 15 Q 75 20, 80 15 Q 85 10, 90 15 M 95 25 Q 100 20, 105 25 T 115 25"
-        stroke="#1C2431"
-        strokeWidth="2"
-        strokeLinecap="round"
-        fill="none"
-        opacity="0.7"
-      />
-    </svg>
+    <img
+      src="/images/hero/arash-signature.png"
+      alt="Arash Nejad signature"
+      className="block h-6 w-auto opacity-80"
+    />
   );
 }
