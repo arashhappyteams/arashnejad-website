c4cb2532af3cbacfde342827871ff1d93a5aa466
arashhappyteams <arash@happyteams.ai>
2026-01-10 18:52:41 -0800

    Remove commented paragraph from About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -274,7 +274,6 @@ export function About() {
                   decoding="async"
                 />
               </div>
-             /*  <p className="font-semibold text-[var(--color-primary)] mb-1 group-hover:text-[var(--color-accent)] transition-colors">BSc & MSc</p> */
               <p className="text-xs text-[var(--color-body-text)] mt-1 mb-0">BSc MSc Electrical & Computer Engineering</p>
             </motion.a>
           </div>
